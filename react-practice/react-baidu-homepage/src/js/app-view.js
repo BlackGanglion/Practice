8d8966a731c24a5b5682b8fe4912996e050720b9
BlackGanglion <BlackGanglion@blackganglion.com>
2016-01-05 02:15:46 +0800

    update react-baidu-homepage
    update react-baidu-homepage

--- react-practice/react-baidu-homepage/src/js/app-view.js
+++ react-practice/react-baidu-homepage/src/js/app-view.js
@@ -6,10 +6,11 @@ import SearchView from './search-view';
 // Create class called AppView that extends the base React Component class
 class AppView extends React.Component {
   render() {
+    let searchPrompt = '百度一下';
     return (
       <div className="container-search">
         <img className="img-logo" src="https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo/bd_logo1_31bdc765.png" alt="" />
-        <SearchView />
+        <SearchView searchPrompt={searchPrompt}/>
       </div>
     );
   }
--- react-practice/react-baidu-homepage/src/js/search-view.js
+++ react-practice/react-baidu-homepage/src/js/search-view.js
@@ -1,11 +1,38 @@
 import React from 'react';
 
 class SearchView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchQuery: 'initSearchQuery'
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    console.log('handleChange', event.target.value);
+    this.setState({
+      searchQuery: event.target.value
+    });
+  }
+  handleClick() {
+    console.log('handleClick', this.state.searchQuery);
+  }
   render() {
     return (
       <div>
-        <input className="input-search" type="text" name="name" value="" />
-        <button className="btn-search" type="button" name="button">百度一下</button>
+        <div>
+          {this.state.searchQuery}
+        </div>
+        <input
+          onChange = {this.handleChange}
+          className="input-search"
+          type="text"
+        ></input>
+        <button
+          onClick={this.handleClick}
+          className="btn-search">
+          {this.props.searchPrompt}
+        </button>
       </div>
     );
   }
