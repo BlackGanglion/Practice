1e03db0fd2e40097020e83e023723dbdb5bff053
BlackGanglion <BlackGanglion@blackganglion.com>
2016-03-26 23:48:19 +0800

    update practice
    update practice

---
+++ javascript-practice/basic/ajax/get.php
@@ -0,0 +1,16 @@
+<?php
+$stuList = array(
+  "0" => array(
+    "name" => "Tom",
+    "age" => 24,
+    "sex" => 1
+  ),
+  "1" => array(
+    "name" => "John",
+    "age" => 23,
+    "sex" => 1
+  )
+);
+
+echo json_encode($stuList);
+?>
